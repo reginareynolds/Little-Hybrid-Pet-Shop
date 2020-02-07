e46f4056a842f10e7823dda395ff167f7f33aaa4
Regina Reynolds <regina.c.reynolds@gmail.com>
2020-02-07 14:18:40 -0500

    Add breed types and characteristics

--- Breeds.py
+++ Breeds.py
@@ -1,6 +1,44 @@
 # Different possible breeds, organized into classes
 
+# TODO: Breed characteristics in ranges?
+
+
 class Bouncer:
     def __init__(self):
         self.size = None
+        self.weight = 0
+        # self.abilities =
+        # self.shape = TODO: pixel shape coordinates
+        # self.sound = TODO: creature sound
+        self.base_value = 0  # TODO: affected by heritage? purebred could be more valuable
+
+
+class Crawler:
+    def __init__(self):
+        self.size = None
+        self.weight = 0
+        # self.abilities =
+        # self.shape =
+        # self.sound =
         self.base_value = 0
+
+
+class Hopper:
+    def __init__(self):
+        self.size = None
+        self.weight = 0
+        # self.abilities =
+        # self.shape =
+        # self.sound =
+        self.base_value = 0
+
+
+class Swimmer:
+    def __init__(self):
+        self.size = None
+        self.weight = 0
+        # self.abilities =
+        # self.shape =
+        # self.sound =
+        self.base_value = 0
+
