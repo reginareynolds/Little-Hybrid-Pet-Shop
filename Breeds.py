9188492f23d93c967b99c65802611a23e2d6b3c7
Regina Reynolds <regina.c.reynolds@gmail.com>
2020-02-07 15:25:19 -0500

    Add set_weight function. Add breed base values. Separate dynamic breed values and constant breed values

--- Breeds.py
+++ Breeds.py
@@ -1,4 +1,6 @@
 # Different possible breeds, organized into classes
+
+# Imported libraries and modules
 import random
 
 
@@ -9,48 +11,70 @@ def set_size(options):
     return(options[choice])
 
 
+# Pick weight from allowable range
+def set_weight(options):
+    return(random.uniform(options[0], options[1]))
+
+
 class Bouncer:
     def __init__(self):
         # Create initial ranges
         self.size = ["Small", "Medium"]
         self.weight = [5.0, 30.0]
-        # self.abilities =
+
+        # Constant breed values
         # self.shape = TODO: pixel shape coordinates
         # self.sound = TODO: creature sound
-        self.base_value = 0  # TODO: affected by heritage? purebred could be more valuable
+        self.base_value = 50  # TODO: affected by heritage? purebred could be more valuable
 
-    def characterize(self):
+    def set_values(self):
         self.size = set_size(self.size)
-
+        self.weight = set_weight(self.weight)
 
 
 class Crawler:
     def __init__(self):
         self.size = ["Small", "Medium", "Large"]
         self.weight = [5.0, 90.0]
-        # self.abilities =
+
+        # Constant breed values
         # self.shape =
         # self.sound =
-        self.base_value = 0
+        self.base_value = 10
+
+    def set_values(self):
+        self.size = set_size(self.size)
+        self.weight = set_weight(self.weight)
 
 
 class Hopper:
     def __init__(self):
         self.size = ["Medium", "Large"]
         self.weight = [30.0, 90.0]
-        # self.abilities =
+
+        # Constant breed values
         # self.shape =
         # self.sound =
-        self.base_value = 0
+        self.base_value = 20
+
+    def set_values(self):
+        self.size = set_size(self.size)
+        self.weight = set_weight(self.weight)
 
 
 class Swimmer:
     def __init__(self):
         self.size = ["Small", "Medium", "Large"]
         self.weight = [5.0, 90.0]
-        # self.abilities =
+
+        # Constant breed values
         # self.shape =
         # self.sound =
-        self.base_value = 0
+        self.base_value = 35
+
+    def set_values(self):
+        self.size = set_size(self.size)
+        self.weight = set_weight(self.weight)
 
 # TODO: Characteristic inherit function?
+# TODO: Add breed abilities
